ab11d5f35a4df19a86ab35c045203d5a9716b8eb
umamahale <umagasmos@gmail.com>
2021-10-02 19:30:50 -0400

    first message

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FindMaximumNumberProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find max Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+        }
+    }
+}
